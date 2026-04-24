71bd5ee67ae5318a30e1bfca8bb02e77d6991d55
Sumanjali07 <Sumanjali07>
2026-04-24 18:34:21 +0530

    update frontend base url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = "https://splitease-backend-0e77.onrender.com";
 console.log("API BASE URL:", BASE_URL);
 
 async function request(path, options = {}) {
