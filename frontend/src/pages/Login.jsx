0594f5a3280a0f453327ecbd72ce3e2adffd8dcf
Sumanjali07 <Sumanjali07>
2026-06-23 13:03:44 +0530

    fix login api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://splitease-backend-0e77.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
